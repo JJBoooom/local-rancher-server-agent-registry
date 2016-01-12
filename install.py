93c096262fd114fbf68a5fe4597a838c8d5399b7
kiongf <kiongf@126.com>
2016-01-12 17:22:51 +0800

    add remote registry

--- install.py
+++ install.py
@@ -15,33 +15,25 @@ import shutil
 from docker import Client
 from fabric.api import *
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - <%(levelname)s> - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - <%(levelname)s> - %(message)s')
 log = logging.getLogger(__name__)
 
+class MyException(Exception):
+    pass
 
 class Registry(object):
     
-    def __init__(self, port, name, store):
+    def __init__(self, port='5000', name='MyPrivateRegistry', store='/var/lib/MyPrivateRegistry'):
         self.port = port
         self.name = name
         self.store = store
-        self.ip = get_hostip()
-
-        if not self.port:
-            self.port = '5000'
-            log.info('use default port:'+self.port)
-        
-        if not self.name:
-            self.name = 'MyPrivateRegistry'
-            log.info('use default image name:'+self.name)
-
-        if not self.store:
-            self.store = '/var/lib/MyPrivateRegistry'
-            log.info('use default image store:'+self.store)
+        self.ip = get_hostip() 
 
     #缺少端口,镜像名以及共享卷合法性认证
     def validate(self):    
-        return self.ip is not None 
+        return (self.ip is not None and self.port is not None and 
+            self.name is not None and
+            self.store is not None)
 
     def display(self):
         log.debug('-----Registry conf-----')
@@ -63,6 +55,9 @@ class Registry(object):
             if result.failed:
                 log.error('Can\'t start docker daemon')
                 return False
+        else:
+            log.error('cancel to start docker daemon?')
+            return False
         return True
 
     def load_images(self, zipped_path, images_file):
@@ -114,51 +109,183 @@ class Registry(object):
             return False
         finally:
             shutil.rmtree(temp)
+
+    def add_registry():
+        docker_conf = '/etc/sysconfig/doccker'
+        if not add_insecure_registry(docker_conf, self.ip, seek.port):
+            sys.exit(1)
     
-#    def run_registry(self):
+    def run_registry(self):
+        with settings(warn_only=True):
+            result = local('docker  run -d --restart=always -p %s:%s --name %s -v %s:/var/lib/registry registry:2' %(self.ip, self.port, self.name, self.store))
+        if result.failed:
+            log.error('run registry fail')
+            return False
+
+        
               
+class  Remote_Registry(Registry):
+    def __init__(self, ip, port='5000', name='MyPrivateRegistry', store='/var/lib/MyPrivateRegistry'):
+        self.port = port
+        self.name = name
+        self.store = store
+        self.ip = ip
+#        self.password = getpass.getpass('Enter a password for %s:' % (self.ip))
+        
+        self.password = 'Cloudsoar123'
+        if not self.validate():
+            log.error('Invalid argument')
+            sys.exit(1)
+        
+        
+    def validate(self):
+        return self.ip != get_hostip() and self.password != None
+
     
+    
+    def check_env(self):
+        env.host_string = "%s:%s"%(self.ip ,22) 
+        env.password = self.password
+        with quiet(), settings(warn_only=True):
+            command = 'which docker'
+            result = run(command, quiet=True)
+            if result.failed:
+                log.error('Docker command isn\'t installed')
+                return False
+        with settings(warn_only=True):
+            command = 'docker version'
+            result = run(command)
+            if result.failed and confirm('docker daemon isn\'t running, Try to start it?'):
+                log.info('Try to start docker daemon')
+                with settings(warn_only=True):
+                    command = 'systemctl start docker'
+                    result = run(command)
+                if result.failed:
+                    log.error('start docker daemon fail')
+                    return False
+                else:
+                    log.info('start docker daemon success')
+            elif not result.failed:
+                pass
+            else:
+                log.info('Cancel to start docker daemon')
+                return False
+        log.info('check enviroment:pass')
+        return True
+    
+    def run_registry(self):
+        env.host_string = "%s:%s"%(self.ip ,22) 
+        env.password = self.password
+        with quiet(), settings(warn_only=True):
+            command = 'docker run -d --restart=always -p %s:5000 --name %s -v %s:/var/lib/registry registry:2' %(self.port, self.name, self.store)
+            log.debug('command:'+command)
+            result = run(command)
+            if result.failed:
+                log.error('run registry fail')
+                return False
+        return True
 
+    def add_registry(self):
+   #     add_insecure_registry(self.ip, self.port)
+        env.host_string = "%s:%s"%(self.ip ,22) 
+        env.password = self.password
+        docker_conf = '/etc/sysconfig/docker'
+        with quiet(), settings(warn_only=True):
+            command = 'grep -e "^"'  
+        
+    
+    def load_images(self, zipped_path, images_file):
+        env.host_string = "%s:%s"%(self.ip ,22) 
+        env.password = self.password
 
-def add_insecure_registry(ip, port):
+        if not zipped_path or not images_file:
+            log.debug('mising arguments')
+            sys.exit(1)
+        if not os.path.exists(zipped_path) or not os.path.exists(images_file):
+            log.error('missing images zipped')
+            sys.exit(1)
+
+        try:
+            tempdir = ''
+            with settings(warn_only=True): 
+                command = 'mktemp -d /tmp/zipped.XXXXXXX'
+                result = run(command)
+              
+                if not result.failed:
+                    tempdir = result.stdout
+                else:
+                    log.error('%s:create tempdir fail' % (self.ip))
+                    system.exit(1)
+
+                result = put(zipped_path, tempdir)
+                if result.failed:
+                    log.error('%s:moving zipped to temp dir fail' % (self.ip))
+                    raise MyException
+
+                command = 'gunzip %s/%s' % (tempdir,os.path.basename(zipped_path))
+                result = run(command)
+                if result.failed:
+                    log.error("%s:gunzip fail"%(self.ip))
+                    raise MyException
+
+                command = 'ls %s' % (tempdir)
+                result = run(command)
+                if result.failed:
+                    log.error("%s:list file faile")%(self.ip)
+                    raise MyException
+                else:
+                    for tfile in result.stdout.split():
+                        command = 'docker load --input %s/%s'%(tempdir, tfile)
+                        result = run(command)
+                        if result.failed:
+                            log.error('%s:load %s fail'%(self.ip, tfile))
+                            raise MyException
+                       
+        except MyException:
+            with settings(warn_only=True): 
+                command = 'rm -rf %s'%(tempdir)
+                result = run(command)
+                sys.exit(1)
+           
+def add_insecure_registry(docker_conf, ip, port):
     content = '--insecure-registry %s:%s' % (ip, port)
-    docker_conf = '/etc/sysconfig/docker'
+   # docker_conf = '/etc/sysconfig/docker'
     if not os.path.exists(docker_conf):
         log.error('missing docker conf')
         sys.exit(1)
 
-    log.debug('start to set')
     try:
-        f = open(docker_conf, 'r')
+        f = open(docker_conf, 'r+')
         try:
-            log.debug('start tot read')
-          
-            for eachline in f:
-                #if re.match(r'^\s*OPTIONS=', eachline, re.M):
-                if re.match(r'^INSECURE_REGISTRY=', eachline, re.M):
-                    irline = eachline
-                    break
+            filedata = file.read()
+            sl = list()
+            for line in filedata.split('\n'):
+                if re.match(r'^INSECURE_REGISTRY=', line.strip(), re.M):
+                    li = line.strip().rstrip('\'')+' --insecure-registry %s:%s\''%(ip, port)
+                elif re.match(r'^#INSECURE_REGISTRY=', line.strip(), re.M):
+                    li = 'INSECURE_REGISTRY=\'--insecure-registry %s:%s\''%(ip, port)
+                else:
+                    li = line
+                sl.append(li)
 
-                if re.match(r'^\s#INSECURE_REGISTRY=', eachline, re.M):
-                    irline = ''
-                    break
-        except Exception,e :
-            log.error('read file fail')
+            newfiledata = '\n'.join(sl)
+            f.seek(0, 0)
+            try:
+                f.write(newfiledata)
+            except Exception, e:
+                log.error('modify %s fail'%(docker_conf))
+                return False
+
+        except Exception,e:
+            log.error('read %s fail'%(docker_conf))
+            return False
         finally:
             f.close()
+
     except Exception, e:
         log.error('open docker conf fail') 
-
-    if irline == '':
-        content2 = content
-    else:
-        content2 = irline.rstrip('\'')+' '+content+'\''
-        print "content2:"+content2
-    with settings(warn_only=True):
-        result = local('sed -i s/^#*INSECURE_REGISTRY=/INSECURE_REGISTRY=\'%s\'/g %s' % (content2, docker_conf))
-    if result.failed:
-        log.error('conigure registry fail')
-        sys.exit(1)
+        return False
+    return True
 
 
 def confirm(msg):
@@ -173,8 +300,6 @@ def confirm(msg):
             return False
         elif choose == 'q':
             sys.exit(1)
-
-
 
        
 def get_hostip():
@@ -220,8 +345,6 @@ def parse_conf(config_path):
     return conf_db
 
 
-def run():
-    pass
 
 def push_private_registry():
     pass
@@ -282,7 +405,17 @@ def xtmain():
 
 
 def main():
+    script_dir_path =os.path.abspath(os.path.dirname(sys.argv[0]))
+    zipped_path = script_dir_path+'/image_zipped.tar.gz'
+    images_file = script_dir_path+'/imagelists'
     #xtmain()
-    add_insecure_registry('192.168.4.2', '5050')
+    #add_insecure_registry('192.168.4.2', '5050')
+    rg = Remote_Registry('192.168.4.30')
+    rg.display()
+    rg.check_env()
+    #rg.run_registry()
+    rg.load_images(zipped_path, images_file)
+    rg.run_registry()
+
 if __name__ == '__main__':
     main()
